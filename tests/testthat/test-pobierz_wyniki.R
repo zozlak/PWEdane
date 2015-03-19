03de540dbbc479ff3a73d8df2c5e60179d7c9941
Mateusz Żółtak <zozlak@zozlak.org>
2015-03-19 11:19:38 +0100

    Zwiększamy jeszcze trochę pokrycie testami.

--- tests/testthat/test-pobierz_wyniki.R
+++ tests/testthat/test-pobierz_wyniki.R
@@ -7,3 +7,9 @@ test_that('pobierz_wyniki działa', {
   expect_more_than(ncol(dane), 10)
   expect_more_than(sum(grepl('^k_', colnames(dane))), 0)
 })
+
+test_that('pobierz_wyniki zgłasza błędy', {
+  expect_error(pobierz_wyniki('sprawdzian', '', 2001), 'W bazie danych brak wskazanych wynikow egzaminu')
+  expect_error(pobierz_wyniki('egzamin gimnazjalny', '', 2001), 'W bazie danych brak wskazanych wynikow egzaminu')
+  expect_error(pobierz_wyniki('matura', '', 2009), 'W bazie danych brak wskazanych wynikow egzaminu')
+})
